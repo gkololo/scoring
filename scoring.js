f58e85e9b7368525f81bd19fa98492c56c9aad56
gkololo <gkoweb974@gmail.com>
2021-10-16 22:35:33 +0400

    avec increment points revoir css

--- scoring.js
+++ scoring.js
@@ -1,4 +1,26 @@
-// function incrementSet1P1 (){
+// function incrementSet1P1 ()
 var i = 0;
-function incSet1P1() {
-        document.getElementById('set1p1').value = ++i;}
+function increment(value, cible) {
+        console.log(value, cible);
+        if(cible === 'set1p1'){
+        document.getElementById('set1p1').value = ++value;
+        }else if (cible === 'set2p1'){
+           document.getElementById('set2p1').value = ++value;
+        }else if (cible === 'set3p1'){
+                document.getElementById('set3p1').value = ++value;
+             }else if (cible === 'pointsp1'){
+                     var afficher = 0;
+                     console.log(typeof value);
+                  if(value == 0){
+                        afficher = 15;
+                  }else if(value == 15){
+                         afficher = 30;
+                  }else if(value == 30){
+                        afficher = 40;
+                }
+                console.log(afficher);
+                document.getElementById('pointsp1').value = afficher;
+             }
+
+
+}
